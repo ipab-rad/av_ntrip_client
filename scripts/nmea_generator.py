d8d1f9a71cf4000ee66fcbb34cb2161e5d90c551
hect95 <hector_cruz95@live.com>
2024-08-28 11:58:33 +0100

    Remove unused functions

--- scripts/nmea_generator.py
+++ scripts/nmea_generator.py
@@ -1,9 +1,7 @@
 """
 NMEA generator module.
 
-This module generates NMEA sentences for GPS data simulation.
-It includes the NMEAGenerator class, which provides methods to generate
-different types of NMEA sentences, such as GPGGA, GPRMC, and GPGST.
+This module generates NMEA (GPGGA) sentence for GPS data simulation.
 """
 
 import time
@@ -11,11 +9,10 @@ import time
 
 class NMEAGenerator:
     """
-    A class to generate NMEA sentences for GPS data simulation.
+    A class to generate NMEA GPGGA sentence.
 
-    The NMEAGenerator class provides methods to generate various NMEA
-    sentences like GGA, RMC, and GST, which are commonly used in GPS data
-    communication.
+    It provides methods to generate and validate GPGGA
+    sentences.
     """
 
     def __init__(self, fix_latitude, fix_longitude, fix_altitude):
@@ -50,49 +47,6 @@ class NMEAGenerator:
 
         checksum = self._calculate_checksum(gga)
         return f'${gga}*{checksum}'
-
-    def generate_rmc_sentence(self):
-        """Generate NMEA RMC sentence."""
-        current_time = time.strftime('%H%M%S', time.gmtime())
-        current_date = time.strftime('%d%m%y', time.gmtime())
-
-        lat_deg = int(self.fix_latitude)
-        lat_min = (self.fix_latitude - lat_deg) * 60
-        lat_hemisphere = 'N' if self.fix_latitude >= 0 else 'S'
-
-        lon_deg = int(abs(self.fix_longitude))
-        lon_min = (abs(self.fix_longitude) - lon_deg) * 60
-        lon_hemisphere = 'E' if self.fix_longitude >= 0 else 'W'
-
-        rmc = (
-            f'GPRMC,{current_time},A,{lat_deg:02d}{lat_min:07.4f},'
-            f'{lat_hemisphere},{lon_deg:03d}{lon_min:07.4f},'
-            f'{lon_hemisphere},000.0,360.0,{current_date},,'
-        )
-
-        checksum = self._calculate_checksum(rmc)
-        return f'${rmc}*{checksum}'
-
-    def generate_gst_sentence(self):
-        """Generate NMEA GST sentence."""
-        current_time = time.strftime('%H%M%S', time.gmtime())
-        # Example values, these would typically come from actual GNSS data
-        rms_err = 0.0
-        semi_major_dev = 0.0
-        semi_minor_dev = 0.0
-        orient = 0.0
-        lat_err_dev = 0.0
-        lon_err_dev = 0.0
-        alt_err_dev = 0.0
-
-        gst = (
-            f'GPGST,{current_time},{rms_err:.1f},{semi_major_dev:.1f},'
-            f'{semi_minor_dev:.1f},{orient:.1f},{lat_err_dev:.1f},'
-            f'{lon_err_dev:.1f},{alt_err_dev:.1f}'
-        )
-
-        checksum = self._calculate_checksum(gst)
-        return f'${gst}*{checksum}'
 
     def is_gpgga_data_valid(self, nmea_sentence: str) -> bool:
         """Validate GPGGA data."""
